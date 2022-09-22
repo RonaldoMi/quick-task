ac77537e4374e959722076bfa4ae94fbbd362a17
Ronaldo <ronaldo.sk7@gmail.com>
2022-09-22 00:44:58 -0300

    Create initial project structure
    1. Added vuetify framework

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
@@ -14,5 +15,6 @@ if (process.env.NODE_ENV === 'development') {
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
