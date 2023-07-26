8aad1f08d50649423381acb7e40ba89fb45fc588
Ronaldo <ronaldo@iliossistemas.com>
2023-07-25 21:16:52 -0300

    update msw
    1. added msw in server production

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store/index'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store/index";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
-  worker.start()
-}
+const { worker } = require("./mocks/browser");
+worker.start();
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
